45aa61dd995d1ee4d97cd1b0dca447871e0ce723
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2023-10-26 19:57:39 -0500

    first error ive made

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,26 @@
 const router = require('express').Router()
 
+
 router.get('/', (req, res) => {
-    res.send('GET /places')
+    let places = [{
+        name: 'Canes',
+        city: 'Chicago',
+        state: 'IL',
+        cuisine: 'Chicken Tenders',
+        pic: 'http://placekitten.com/250/250'
+    }, {
+        name: 'Taco Max',
+        city: 'Chicago',
+        state: 'IL',
+        cuisine: 'Mexican',
+        pic: 'http://placekitten.com/250/250'
+    },{
+        name: 'Happy China',
+        city: 'Chicago',
+        state: 'IL',
+        cuisine: 'asian',
+        pic: 'http://placekitten.com/250/250'
+    }]
+    res.render('places/index', { places })
 })
 module.exports = router
