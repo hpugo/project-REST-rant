eb37028309b5322dd88663d81b1f171c9f34137d
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2023-11-04 18:04:48 -0500

    issues adding post route and then issues with places controller

--- controllers/places.js
+++ controllers/places.js
@@ -1,33 +1,56 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
+
+router.get ('/', (req,res) => {
+    res.render('places/index', {places})
+})
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
-// router.post<'/new', (req, res) => {
-//     res.render('places/new')
-// }
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'Canes',
-        city: 'Chicago',
-        state: 'IL',
-        cuisine: 'Chicken Tenders',
-        pic: '/images/canes.jpeg'
-    }, {
-        name: 'Taco Max',
-        city: 'Chicago',
-        state: 'IL',
-        cuisine: 'Mexican',
-        pic: 'http://placekitten.com/250/250'
-    },{
-        name: 'Happy China',
-        city: 'Chicago',
-        state: 'IL',
-        cuisine: 'asian',
-        pic: 'http://placekitten.com/250/250'
-    }]
-    res.render('places/index', {places})
+
+router.post ('/new', (req, res) => {
+    res.send('POST /places')
 })
-module.exports = router
+
+// router.get('/', (req, res) => {
+//     let places = [{
+//         name: 'Canes',
+//         city: 'Chicago',
+//         state: 'IL',
+//         cuisine: 'Chicken Tenders',
+//         pic: '/images/canes.jpeg'
+//     }, {
+//         name: 'Taco Max',
+//         city: 'Chicago',
+//         state: 'IL',
+//         cuisine: 'Mexican',
+//         pic: 'http://placekitten.com/250/250'
+//     },{
+//         name: 'Happy China',
+//         city: 'Chicago',
+//         state: 'IL',
+//         cuisine: 'asian',
+//         pic: 'http://placekitten.com/250/250'
+//     }]
+//     res.render('places/index', {places})
+// })
+// module.exports = router
+
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+    if (!req.body.pic) {
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.state = 'anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = "USA"
+    }
+    places.push(req.body)
+    res.redirect('/places')
+})
---
+++ models/places.js
@@ -0,0 +1,21 @@
+
+
+module.exports = [{
+    name: 'Canes',
+    city: 'Chicago',
+    state: 'IL',
+    cuisine: 'Chicken Tenders',
+    pic: '/images/canes.jpeg'
+}, {
+    name: 'Taco Max',
+    city: 'Chicago',
+    state: 'IL',
+    cuisine: 'Mexican',
+    pic: 'http://placekitten.com/250/250'
+},{
+    name: 'Happy China',
+    city: 'Chicago',
+    state: 'IL',
+    cuisine: 'asian',
+    pic: 'http://placekitten.com/250/250'
+}]
