e501c3678a4f6f11c59d85c899832bbd3bef13d5
Henry Pugo <henry@Henrys-MacBook-Pro-2.local>
2023-11-05 12:02:18 -0600

    delete function added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 
 
@@ -8,7 +9,7 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-
+app.use(methodOverride('_method'))
 
 
 app.get('/', (req, res) => {
